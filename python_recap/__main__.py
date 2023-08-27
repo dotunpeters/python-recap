5f2ce300524f5357b92f9904eec374e1812e6c8c
dotunpeters <oyelakindotun@gmail.com>
2023-08-27 03:45:25 +0100

    initial commit

---
+++ python_recap/__main__.py
@@ -0,0 +1,11 @@
+def main(message: str) -> None:
+    print(f"message: {message}", len(message))
+
+
+def fibonacci_sequence():
+    # write fibonacci sequence
+    print("fibonacci sequence")
+
+
+if __name__ == "__main__":
+    main("Greetings from main package")
---
+++ python_recap/pr_variables.py
@@ -0,0 +1,3 @@
+from .__main__ import main
+
+main("Greetings from pr_var module")
